4e904790b627fefa0b30f7a1e7be00c5bb976b6e
Kenneth Phang <bunnyppl@gmail.com>
2019-08-16 10:14:43 +0800

    fix test case

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -10,13 +10,13 @@ namespace AspNetCoreWebServiceTest.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!", response.output);
+            Assert.Equal("2Hello World!", response.output);
         }
 
         [Theory]
-        [InlineData(null, "Hello !")]
-        [InlineData("", "Hello !")]
-        [InlineData("AWS CodeStar", "Hello AWS CodeStar!")]
+        [InlineData(null, "2Hello !")]
+        [InlineData("", "2Hello !")]
+        [InlineData("AWS CodeStar", "2Hello AWS CodeStar!")]
         public void InputParamGetResponseTest(string inputValue, string expectedOutput)
         {
             HelloController controller = new HelloController();
@@ -29,13 +29,13 @@ namespace AspNetCoreWebServiceTest.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Post().Value as Response;
-            Assert.Equal("Hello World!", response.output);
+            Assert.Equal("2Hello World!", response.output);
         }
 
         [Theory]
-        [InlineData(null, "Hello !")]
-        [InlineData("", "Hello !")]
-        [InlineData("AWS CodeStar", "Hello AWS CodeStar!")]
+        [InlineData(null, "2Hello !")]
+        [InlineData("", "2Hello !")]
+        [InlineData("AWS CodeStar", "2Hello AWS CodeStar!")]
         public void InputParamPostResponseTest(string inputValue, string expectedOutput)
         {
             HelloController controller = new HelloController();
